379ad95edfd736e453eedc331b0522dc50d65ef0
DARKEST OWL HACKERS <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-09-19 23:56:02 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -2,7 +2,6 @@ import os, sys, platform,time
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
-    os.system('git pull')
     import IG   
 elif bit == '32bit':
     os.system('clear')
