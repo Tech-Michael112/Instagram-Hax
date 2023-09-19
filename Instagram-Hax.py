d98c26ac14ab2498f1536f61924e6c36855f98da
DARKEST OWL HACKERS <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-09-19 23:21:04 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -1,2 +1,10 @@
-import igc
-igc.Instagram()
+import os, sys, platform,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import IG   
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import igc
