1ef2d8a5a2710091083dc14962fdd5d8d942c3db
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-06-05 17:18:09 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -1,9 +1,6 @@
-import os, sys, platform,time
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print('\n Your device is 64 bit')
-    os.system('clear')
-    os.system('git pull')
-    os.system('python gram1_enc.py')
-else:
-    exit(" your device is not 64 ")
+
+    
+os.system('clear')
+    
+s.system('git pull')
+import gram1
