74a8afd7312e8bc9f539f5ba468c6ba8ab8b27b4
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-07-18 16:25:13 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -1,6 +1,1 @@
-import os 
-    
-os.system('clear')
-    
-os.system('git pull')
-import gram1
+print("\33[1;32m sorry Instagram is Currently on fixing \n dont worry your subscription key won't be removed! as you think")
