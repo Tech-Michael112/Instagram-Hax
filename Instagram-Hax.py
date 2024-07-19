8d7c1fb6ff8781ee7f84804b46c8b07f189f533a
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-07-19 23:32:31 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -1,1 +1,15 @@
-print("\33[1;32m sorry Instagram is Currently on fixing \n dont worry your subscription key won't be removed! as you think")
+import os
+import requests
+import logging 
+
+git = 'git pull'
+def Execution():
+    try:
+        os.system(git)
+        import gram1
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+        print("There's an exception!!!")
+    except ImportError as e:
+        print("Error importing gram1 module: dont edit repository document...", e)
+
+Execution()
