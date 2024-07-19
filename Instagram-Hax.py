cd843adf713a192d8a48a7db2637a08b97d49991
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-07-19 23:37:50 +0100

    Update Instagram-Hax.py

--- Instagram-Hax.py
+++ Instagram-Hax.py
@@ -2,14 +2,18 @@ import os
 import requests
 import logging 
 
+error_color=f'\33[1;91m'
+success_color=f'\033[1;32m'
+line=f'"_"*53'
+
 git = 'git pull'
 def Execution():
     try:
         os.system(git)
         import gram1
-    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
-        print("There's an exception!!!")
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as error:
+        print(f"There's an exception!!! error {error}")
     except ImportError as e:
-        print("Error importing gram1 module: dont edit repository document...", e)
-
+        
+        print(f"{error_color} dont edit the module documentation.... or author has not uploaded update yet \n{line}")
 Execution()
