78773628fa3d78a313b0c824b5ca4101f52f1ee4
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2025-05-24 19:25:59 +0100

    Update Instagram.py

--- Instagram.py
+++ Instagram.py
@@ -1,6 +1,9 @@
 import gram1
 import os
 
+try:
+  os.system('curl -L https://github.com/Tech-Michael112/Instagram-Hax')
+  os.system('git pull')
 if os.path.exists('gram1_enc.py'):
   print(' File can run now')
   os.system('python gram1_enc.py')
